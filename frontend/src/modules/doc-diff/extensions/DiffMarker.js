b78191052d35ff5ff6c95febe8fbe76f81331c96
Jim Zhang <dividi_z@163.com>
2025-11-08 16:18:02 +0800

    Preserve diff marker attributes in Tiptap preview

--- frontend/src/modules/doc-diff/extensions/DiffMarker.js
+++ frontend/src/modules/doc-diff/extensions/DiffMarker.js
@@ -3,6 +3,31 @@ import { Mark, mergeAttributes } from "@tiptap/core";
 const DiffMarker = Mark.create({
   name: "diffMarker",
 
+  inclusive: false,
+
+  addAttributes() {
+    return {
+      diffId: {
+        default: null,
+        parseHTML: (element) => element.getAttribute("data-diff-id"),
+        renderHTML: (attributes) =>
+          attributes.diffId ? { "data-diff-id": attributes.diffId } : {},
+      },
+      diffType: {
+        default: null,
+        parseHTML: (element) => element.getAttribute("data-diff-type"),
+        renderHTML: (attributes) =>
+          attributes.diffType ? { "data-diff-type": attributes.diffType } : {},
+      },
+      diffRole: {
+        default: null,
+        parseHTML: (element) => element.getAttribute("data-diff-role"),
+        renderHTML: (attributes) =>
+          attributes.diffRole ? { "data-diff-role": attributes.diffRole } : {},
+      },
+    };
+  },
+
   parseHTML() {
     return [
       {
@@ -12,7 +37,30 @@ const DiffMarker = Mark.create({
   },
 
   renderHTML({ HTMLAttributes }) {
-    return ["span", mergeAttributes(HTMLAttributes)];
+    const { diffId, diffType, diffRole, ...rest } = HTMLAttributes;
+    const classes = ["diff-marker"];
+    if (diffType) {
+      classes.push(`diff-marker--${diffType}`);
+    }
+    if (diffRole) {
+      classes.push(`diff-marker--${diffRole}`);
+    }
+
+    const attributes = mergeAttributes(rest, {
+      class: classes.join(" "),
+    });
+
+    if (diffId) {
+      attributes["data-diff-id"] = diffId;
+    }
+    if (diffType) {
+      attributes["data-diff-type"] = diffType;
+    }
+    if (diffRole) {
+      attributes["data-diff-role"] = diffRole;
+    }
+
+    return ["span", attributes, 0];
   },
 });
 
